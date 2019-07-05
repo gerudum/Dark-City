548ec18f864fef1c1d4281d36c55e687c331c134
gerudum <49799704+gerudum@users.noreply.github.com>
2019-07-05 14:37:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -317,7 +317,7 @@ bot.on('message', message=> {
             var listing = "";
             var index = 0;
             for (var i = 1; i < args.length; i++) {
-                    if(args[i] != args[args.length]){
+                    if(args[i + 1] != args[args.length]){
                         listing += args[i].toString();
                     }
                   
