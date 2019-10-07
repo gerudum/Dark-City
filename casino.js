dd054e5da31a1c18ecc419fa0ef6ab868fffb178
GeruGames <49799704+gerudum@users.noreply.github.com>
2019-10-07 12:57:29 -0400

    Casino

--- casino.js
+++ casino.js
@@ -1,9 +1,11 @@
 export function Roll(loot, weights){
     var top = 0;
     var total = 0;
+    
     for(var j = 0; j < weights.length; j++){
         total+=weights[j];
     }
+    
     var rand = Math.floor(Math.random() * total);
     for(var i = 0; i < loot.length; i++){
         top+=weights[i]; 
@@ -14,8 +16,8 @@ export function Roll(loot, weights){
 }
 
 export class Entry (){
-  constructor(result,entry){
-      this.result = result;
-      this.entry = entry;
-  }
+    constructor(result,weight){
+        this.result = result;
+        this.weight = weight;
+    }
 }
