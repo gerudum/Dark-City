8d4b53a6d0e0509c0849ef13169d13d2839b9930
GeruGames <49799704+gerudum@users.noreply.github.com>
2019-10-07 12:38:39 -0400

    Organization

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const app = express();
 const fs = require('fs');
 const prefix = "/";
 
+import Player from './player';
+import Listing from './listing';
 
 app.get("/", (request, response) => {
   console.log(Date.now() + " Ping Received");
@@ -33,21 +35,6 @@ var conditions = require("./conditions.js");
 setInterval(function() {
 	Update();
 }, 10000);
-
-class Player {
-	construcor(id,name,art,level = 0, experience = 0, coins = 0, points = 0, weight = 0, gain = 0, collection = new Date()){
-		this.id = id;
-		this.name = name;
-		this.art =art;
-		this.level = level;
-		this.experience = experience;
-		this.coins = coins;
-		this.points = points;
-		this.weight = weight;
-		this.gain = gain;
-		this.collection = collection;
-	}
-}
 
 class DepotListing {
 	constructor(id = 0, name, price, attachment, startDate, endDate){
