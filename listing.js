8c39e3f54ce0c1d472b526774f3b94243f295330
GeruGames <49799704+gerudum@users.noreply.github.com>
2019-10-07 12:40:35 -0400

    Depot Listing Class Organization

--- listing.js
+++ listing.js
@@ -1,1 +1,10 @@
-
+export default class DepotListing {
+	constructor(id = 0, name, price, attachment, startDate, endDate){
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.attachment = attachment;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+}
