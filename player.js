a0e4eab04f359ee71aa89310d65a11ba7e07a1b3
GeruGames <49799704+gerudum@users.noreply.github.com>
2019-10-07 12:39:51 -0400

    Player Class

--- player.js
+++ player.js
@@ -1,1 +1,14 @@
-
+export default class Player {
+	construcor(id,name,art,level = 0, experience = 0, coins = 0, points = 0, weight = 0, gain = 0, collection = new Date()){
+		this.id = id;
+		this.name = name;
+		this.art =art;
+		this.level = level;
+		this.experience = experience;
+		this.coins = coins;
+		this.points = points;
+		this.weight = weight;
+		this.gain = gain;
+		this.collection = collection;
+	}
+}
